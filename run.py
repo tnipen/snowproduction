1635ada1f5b4f7b8a9583820cbff318cea37dd0f
Thomas Nipen <tnipen@gmail.com>
2017-02-23 12:53:36 +0000

    Added -month

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ import mpl_toolkits.basemap
 import argparse
 
 __version__ = "0.1.0"
+__days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
 def plot(lats, lons, values, args):
@@ -82,7 +83,7 @@ def snow_production(temperature, rh, threshold=0):
     return wetbulb(temperature, rh) < threshold
 
 
-def save(lats, lons, values, filename):
+def save(lats, lons, values, filename, x=None, y=None, proj=None):
     """
     Creates a netcdf file with snow production values
 
@@ -93,29 +94,52 @@ def save(lats, lons, values, filename):
         filename (str): Write to this filename
     """
 
-    file = netCDF4.Dataset(filename, 'w', format="NETCDF4")
+    file = netCDF4.Dataset(filename, 'w', format="NETCDF3_CLASSIC")
     file.createDimension("x", lats.shape[1])
     file.createDimension("y", lats.shape[0])
     vLat=file.createVariable("latitude", "f4", ("y", "x"))
     vLon=file.createVariable("longitude", "f4", ("y", "x"))
     vValues=file.createVariable("values", "f4", ("y", "x"))
+    if x is not None:
+        vX=file.createVariable("x", "f4", ("x"))
+    if y is not None:
+        vY=file.createVariable("y", "f4", ("y"))
+    if proj is not None:
+        vProj=file.createVariable("proj", "string", None)
 
     vLat[:] = lats
+    vLat.units = "degrees_north" ;
     vLon[:] = lons
+    vLon.units = "degrees_east" ;
     vValues[:] = values
     vValues.units = "hours"
+    if x is not None:
+        vX[:] = x
+    if y is not None:
+        vY[:] = y
+    if proj is not None:
+        vProj[:] = proj
     file.close()
 
 
-def get_values(dates, threshold):
+def get_values(args):
     lats = None
     lons = None
+    x = None
+    y = None
+    proj = None
     tindex = range(6, 30)
 
-    total = None
-    count = 0
-    for date in dates:
-        print date
+    if args.month:
+        # Store values for each month
+        total = [None]*12
+        count = [0]*12
+    else:
+        total = None
+        count = 0
+    for date in args.dates:
+        if args.debug:
+            print date
         year = int(date / 10000)
         month = int(date / 100) % 100
         day = int(date % 100)
@@ -135,37 +159,64 @@ def get_values(dates, threshold):
             dir = "/lustre/storeB/immutable/short-term-archive/DNMI_AROME_METCOOP/"
             ifilename = "%s/%04d/%02d/%02d/AROME_MetCoOp_00_fp.nc_%d" % (dir, year, month, day, date)
         try:
-            print ifilename
             ifile = netCDF4.Dataset(ifilename, 'r')
-            print 1
             if lats is None:
                 lats = verif.util.clean(ifile.variables["latitude"])
                 lons = verif.util.clean(ifile.variables["longitude"])
+                # x = verif.util.clean(Ifile.variables["x"])
+                # y = verif.util.clean(Ifile.variables["y"])
+                # proj = verif.util.clean(Ifile.variables["proj"])
             t2 = verif.util.clean(ifile.variables["air_temperature_2m"][tindex, :, :, :])-273.15
             rh2 = verif.util.clean(ifile.variables["relative_humidity_2m"][tindex, :, :, :])
-            curr_hours = snow_production(t2, rh2, threshold)
+            curr_hours = snow_production(t2, rh2, args.threshold)
             curr_hours = np.squeeze(np.sum(curr_hours, axis=0))
-            if total is None:
-                total = curr_hours
+            if args.month:
+                m = month - 1
+                if total[m] is None:
+                    total[m] = curr_hours
+                else:
+                    total[m] += curr_hours
+                count[m] += 1
             else:
-                total += curr_hours
-            count += 1
+                if total is None:
+                    total = curr_hours
+                else:
+                    total += curr_hours
+                count += 1
             ifile.close()
         except Exception as e:
             print "Could not parse %s" % ifilename
             print e
 
-    total = total * 1.0 / count * 365
-    return [lats, lons, total]
+    if args.month:
+        hours = None
+        for m in range(0, 12):
+            if total[m] is None:
+                verif.util.warning("Missing data for month %d" % (m + 1))
+            else:
+                if hours is None:
+                    hours = np.zeros(total[m].shape, int)
+                print "Values for month %d: %d" % (m + 1, count[m])
+                hours += total[m] * __days_in_month[m] / count[m]
+    else:
+        hours = total * 1.0 / count * 365
+    return [lats, lons, hours]
 
 def main():
+    def parse_dates(string):
+        dates = verif.util.parse_numbers(string, True)
+        dates = [int(date) for date in dates]
+        return dates 
+
     parser = argparse.ArgumentParser(description="Create maps of snow production potential")
     parser.add_argument('--version', action="version", version=__version__)
     subparsers = parser.add_subparsers(title="valid commands", dest="command")
     p_compute = subparsers.add_parser('compute', help='Compute hours')                                         
-    p_compute.add_argument('-d', type=str, help='Dates', dest="dates")
+    p_compute.add_argument('-d', help='Dates', dest="dates", type=parse_dates)
     p_compute.add_argument('-r', required=True, type=float, help='Threshold (degrees C)', dest="threshold")
     p_compute.add_argument('-f', type=str, help='Output filename', dest="filename")
+    p_compute.add_argument('-month', help='Compute each month, then sum', action="store_true")
+    p_compute.add_argument('-debug', help="Show debug information", action="store_true")
 
     p_plot = subparsers.add_parser('plot', help='Plot hours')
     p_plot.add_argument('-f', type=str, help='Input filename', dest="filename")
@@ -174,12 +225,12 @@ def main():
     p_plot.add_argument('-edges', help='Colorbar edges', type=verif.util.parse_numbers)
     p_plot.add_argument('-cmap', help='Colormap', type=str)
     p_plot.add_argument('-maptype', help='maptype', type=str)
+    p_plot.add_argument('-debug', help="Show debug information", action="store_true")
 
     args = parser.parse_args()
 
     if args.command == "compute":
-        dates = verif.util.parse_numbers(args.dates, True)
-        [lats, lons, values] = get_values(dates, args.threshold)
+        [lats, lons, values] = get_values(args)
         save(lats, lons, values, args.filename)
     elif args.command == "plot":
         [lats, lons, values] = load_finished_file(args.filename)
